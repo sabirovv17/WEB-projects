8b086289fc402c0e8ef1ff06602484c8e85ac1ca
Амир <amir@MacBook-Pro--Amir.local>
2025-03-24 12:14:45 +0300

    rm spring

--- WEB-SPRING/backend/src/main/java/sabirov/itmo/web4/dao/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package sabirov.itmo.web4.dao.repository;
-
-import sabirov.itmo.web4.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
